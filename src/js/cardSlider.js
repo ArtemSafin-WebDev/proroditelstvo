d2a5944cc5a991f4c9c84ea04e39a5fa37e6b60d
Артем Сафин <artemsafin.webdev@gmail.com>
2020-04-16 00:07:36 +0300

    Базовая версия мобильного слайдера

--- src/js/cardSlider.js
+++ src/js/cardSlider.js
@@ -18,4 +18,110 @@ export default function() {
             }
         });
     });
+
+    // Mobile slider
+
+    const mobileCardSliderElements = Array.from(document.querySelectorAll('.js-mobile-card-slider'));
+
+    mobileCardSliderElements.forEach(element => {
+        const slides = Array.from(element.querySelectorAll('.card-slider__mobile-slider-slide'));
+
+        const slidesCount = slides.length;
+
+        let activeSlideIndex = 0;
+        let sliderAnimating = false;
+        const prev = element.querySelector('.card-slider__mobile-arrow--prev');
+        const next = element.querySelector('.card-slider__mobile-arrow--next');
+
+        let state = {
+
+        }
+        
+
+        function setActiveSlide(index) {
+          
+
+            if (slidesCount <= 1) return;
+
+            let currentSlide = slides[index];
+            let prevSlide = null;
+            let nextSlide = null;
+
+            if (slidesCount < 3) {
+                nextSlide = slides[index - 1] ? slides[index - 1] : slides[index + 1];
+            } else {
+                if (index < slidesCount - 1) {
+                    nextSlide = slides[index + 1];
+                } else {
+                    nextSlide = slides[0];
+                }
+
+                if (index > 0) {
+                    prevSlide = slides[index - 1];
+                } else {
+                    prevSlide = slides[slidesCount - 1];
+                }
+            }
+
+
+            state = {
+                currentSlide,
+                prevSlide,
+                nextSlide
+            };
+
+
+            slides.forEach(slide => {
+                slide.style.transform = '';
+                slide.style.zIndex = '';
+                if (slide === currentSlide) {
+                    slide.style.transform = '';
+                    slide.style.zIndex = 10;
+                } else if (slide === nextSlide) {
+                    slide.style.transform = `translateY(3rem) scale(.9)`;
+                    slide.style.zIndex = 8;
+                } else {
+                    slide.style.transform = `translateY(6rem) scale(.8)`;
+                    slide.style.zIndex = 4;
+                }
+            });
+
+
+            activeSlideIndex = index;
+
+            console.log(state);  
+        }
+
+        setActiveSlide(activeSlideIndex);
+
+
+        function nextSlide() {
+            if (activeSlideIndex < slidesCount - 1) {
+                setActiveSlide(activeSlideIndex + 1);
+            } else {
+                setActiveSlide(0);
+            }
+        }
+
+        function prevSlide() {
+            if (activeSlideIndex > 0) {
+                setActiveSlide(activeSlideIndex - 1);
+            } else {
+                setActiveSlide(slidesCount - 1);
+            }
+        }
+
+
+        prev.addEventListener('click', event => {
+            event.preventDefault();
+            prevSlide();
+        })
+        next.addEventListener('click', event => {
+            event.preventDefault();
+            nextSlide();
+        })
+
+        
+
+    });
 }
--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import polyfills from './polyfills';
 import detectTouch from './detectTouch';
 import cardSlider from './cardSlider';
 
+
 document.addEventListener('DOMContentLoaded', function() {
     polyfills();
     detectTouch();
