c3edd84150bd4de11e188a51e14b61d7c7000b52
Artem Safin <artemsafin.webdev@gmail.com>
2020-04-30 15:41:53 +0300

    Закончена страница курсов

--- src/js/allCoursesSliders.js
+++ src/js/allCoursesSliders.js
@@ -15,10 +15,9 @@ export default function() {
                 prevEl: element.querySelector('.all-courses__slider-arrow--prev')
             },
             breakpoints: {
-
                 569: {
                     slidesPerView: 1,
-                    spaceBetween: 60,
+                    spaceBetween: 60
                 },
                 1025: {
                     slidesPerView: 1,
@@ -26,7 +25,38 @@ export default function() {
                 },
                 1201: {
                     slidesPerView: 1,
-                    spaceBetween: 108,
+                    spaceBetween: 108
+                }
+            }
+        });
+    });
+
+    const smallCoursesSliders = Array.from(document.querySelectorAll('.js-small-courses-slider'));
+
+    smallCoursesSliders.forEach(element => {
+        const container = element.querySelector('.swiper-container');
+
+        new Swiper(container, {
+            slidesPerView: 'auto',
+            watchOverflow: true,
+            spaceBetween: 18,
+            navigation: {
+                nextEl: element.querySelector('.all-courses__small-slider-arrow--next'),
+                prevEl: element.querySelector('.all-courses__small-slider-arrow--prev')
+            },
+            breakpoints: {
+                569: {
+                    slidesPerView: 2,
+                    spaceBetween: 24
+                },
+                969: {
+                    slidesPerView: 3,
+                    spaceBetween: 32
+                },
+               
+                1201: {
+                    slidesPerView: 3,
+                    spaceBetween: 48
                 }
             }
         });
